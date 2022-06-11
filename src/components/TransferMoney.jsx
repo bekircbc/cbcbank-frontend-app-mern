a0022f413e1257357eee27ebd560e731a2d9c2a3
Bekir - CDI <b.burakcebecii@gmail.com>
2022-06-11 02:44:57 +0200

    transfer money finished with custom hook..

--- src/components/TransferMoney.jsx
+++ src/components/TransferMoney.jsx
@@ -1,25 +1,47 @@
 import { useContext, useRef } from "react";
 import { AppContext } from "../AppContext";
+import useTransferMoney from "../hooks/useTransferMoney";
 
 export const TransferMoney = () => {
-  const idInputRef = useRef();
-  const pinInputRef = useRef();
+  const receiverIdRef = useRef();
+  const receiverIbanRef = useRef();
+  const receiverOwnerRef = useRef();
+  const amountRef = useRef();
   const {
     isLoggedIn,
     // isLoggedIn,
     //   isLoggedInHandler,
   } = useContext(AppContext);
   return (
-    <>
+    <div className="login">
       {isLoggedIn ? (
         <>
-          <h1>Transfer Money</h1>
+          <h1>Login</h1>
+
+          <form className="formCard">
+            <label htmlFor="id">Receiver Id</label>
+            <input type="text" required id="id" ref={receiverIdRef} />
+
+            <label htmlFor="IBAN">Receiver IBAN</label>
+            <input type="text" required id="IBAN" ref={receiverIbanRef} />
+
+            <label htmlFor="owner">Receiver Owner</label>
+            <input type="text" required id="owner" ref={receiverOwnerRef} />
+            <label htmlFor="amount">Amount</label>
+            <input type="number" required id="amount" ref={amountRef} />
+            <button
+              className="btnTransferMoney"
+              onClick={() => useTransferMoney}
+            >
+              Transfer Money
+            </button>
+          </form>
         </>
       ) : (
         <>
-          <h1>Transfer Moneyy</h1>
+          <h1>Login first to transfer money.</h1>
         </>
       )}
-    </>
+    </div>
   );
 };
